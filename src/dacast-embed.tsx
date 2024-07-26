c07d8eeacf8ccc04ceb3f2dd574e9804743eb2e7
Carter Fritsch <carter.fyi@outlook.com>
2024-07-26 15:56:48 -0700

    MVP Player Implementation

--- src/dacast-embed.tsx
+++ src/dacast-embed.tsx
@@ -11,17 +11,72 @@
  * limitations under the License.
  */
 
-import React, { ReactElement } from "react";
-import { BlockAttributes } from "widget-sdk";
+import { BlockAttributes } from '@staffbase/widget-sdk';
+import React, { useEffect, ReactElement, useState } from 'react';
 
-/**
- * React Component
- */
 export interface DacastEmbedProps extends BlockAttributes {
-  contentid: string;
+  contentid: string; // https://player.dacast.com/js/player.js?contentId={contentid}
 }
 
+
 export const DacastEmbed = ({ contentid }: DacastEmbedProps): ReactElement => {
-  return <div>Dacast Content ID: {contentid} </div>;
+
+  const [playerHidden, setPlayerHidden] = useState(true);
+
+
+  const handleScriptLoad = () => {
+    // Access the global function here
+    if (window.dacast) {
+      // Call the global function or perform any actions needed
+      try {
+        window.dacast(contentid, "dacast-player"); // https://www.dacast.com/player-api-documentation/#aGettingstarted-UsingtheAPI
+        setPlayerHidden(false);
+      }
+      catch (e) {
+        console.error('DacastEmbed: Error Loading Dacast Player', e);
+      }
+    }
+  };
+
+  useEffect(() => {
+
+    if (!contentid) {
+      console.info('DacastEmbed: No content ID provided');
+      return;
+    }
+    try {
+      console.info(`DacastEmbed: Loading Dacast Player with Content ID: ${contentid}`);
+      const script = document.createElement('script');
+      script.src = `https://player.dacast.com/js/player.js?contentId=${contentid}`;
+      script.async = true;
+      script.addEventListener('load', handleScriptLoad);
+      document.head.appendChild(script);
+
+      // Clean Up on Unmount
+      return () => {
+        script.removeEventListener('load', handleScriptLoad);
+        document.head.removeChild(script);
+      };
+    } catch (e) {
+      console.error('DacastEmbed: Error loading Dacast Player Script', e);
+
+    }
+  }, [contentid]);
+
+  return <div style={{
+    minWidth: "400px",
+    minHeight: "300px",
+    overflow: "hidden",
+    height: "100%",
+  }}>
+
+    <div id="dacast-player" hidden={playerHidden} style={{ position: 'relative' }}></div>
+  </div>
+    ;
 };
 
+declare global {
+  interface Window {
+    dacast?: (content_id: string, container: HTMLElement | string, options?: any) => void;
+  }
+}
